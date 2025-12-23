8401cc7e67a3fa96bf50c6e2b4f0a2413340c412
Shivakumar-05-g <gorigeshivakumar8@gmail.com>
2025-12-23 11:45:31 +0530

    commit

---
+++ app_linearRegression.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
